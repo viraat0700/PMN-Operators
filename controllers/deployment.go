6f90b0649e8e1078a3a41138f405a759a95966b0
root <root@MSI>
2024-12-30 12:31:45 +0530

    added CM file and pvc files

--- controllers/deployment.go
+++ controllers/deployment.go
@@ -5133,7 +5133,7 @@ func (r *PmnsystemReconciler) orc8rPrometheusNginxProxyDeployment(cr *v1.Pmnsyst
 		{
 			Name:      "prometheus-certs",
 			MountPath: "/etc/nginx/conf.d/prometheus.crt",
-			ReadOnly: true,
+			ReadOnly:  true,
 			SubPath:   "prometheus.crt",
 		},
 		{
@@ -5145,7 +5145,7 @@ func (r *PmnsystemReconciler) orc8rPrometheusNginxProxyDeployment(cr *v1.Pmnsyst
 			Name:      "prometheus-certs",
 			MountPath: "/etc/nginx/conf.d/prometheus-ca.crt",
 			SubPath:   "prometheus-ca.crt",
-			ReadOnly: true,
+			ReadOnly:  true,
 		},
 	}
 
@@ -5278,5 +5278,210 @@ func (r *PmnsystemReconciler) orc8rPrometheusNginxProxyDeployment(cr *v1.Pmnsyst
 		terminationMessagePolicy,       // Termination message policy
 		image,                          // Image
 		nil,                            // Affinity
+	)
+}
+func (r *PmnsystemReconciler) orc8rUserGrafanaDeployment(cr *v1.Pmnsystem) *appsv1.Deployment {
+	int64Ptr := func(i int64) *int64 { return &i }
+	int32Ptr := func(i int32) *int32 { return &i }
+
+	labels := map[string]string{
+		"app": "orc8r-user-grafana",
+	}
+
+	// Define volumes in a separate variable
+	volumes := []corev1.Volume{
+		{
+			Name: "config",
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "grafana-config-file",
+					},
+					DefaultMode: int32Ptr(420),
+				},
+			},
+		},
+		{
+			Name: "datasources",
+			VolumeSource: corev1.VolumeSource{
+				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					ClaimName: "grafanadatasources",
+				},
+			},
+		},
+		{
+			Name: "dashboardproviders",
+			VolumeSource: corev1.VolumeSource{
+				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					ClaimName: "grafanaproviders",
+				},
+			},
+		},
+		{
+			Name: "dashboards",
+			VolumeSource: corev1.VolumeSource{
+				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					ClaimName: "grafanadashboards",
+				},
+			},
+		},
+		{
+			Name: "grafana-data",
+			VolumeSource: corev1.VolumeSource{
+				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					ClaimName: "grafanadata",
+				},
+			},
+		},
+	}
+
+	// VolumeMounts
+	volumeMounts := []corev1.VolumeMount{
+		{
+			Name:      "config",
+			MountPath: "/etc/grafana",
+		},
+		{
+			Name:      "dashboards",
+			MountPath: "/var/lib/grafana/dashboards",
+		},
+		{
+			Name:      "datasources",
+			MountPath: "/etc/grafana/provisioning/datasources/",
+		},
+		{
+			Name:      "dashboardproviders",
+			MountPath: "/etc/grafana/provisioning/dashboards/",
+		},
+		{
+			Name:      "grafana-data",
+			MountPath: "/var/lib/grafana",
+		},
+	}
+
+
+	// Define the securityContext for the container
+	securityContext := &corev1.SecurityContext{
+		Privileged: func(b bool) *bool { return &b }(true),
+	}
+
+	// If Bevo is true, add the NET_ADMIN capability
+	// if cr.Spec.Bevo {
+	// 	securityContext.Capabilities = &corev1.Capabilities{
+	// 		Add: []corev1.Capability{"NET_ADMIN"},
+	// 	}
+	// }
+
+	// Define imagePullSecrets
+	// imagePullSecrets := []corev1.LocalObjectReference{
+	// 	{Name: cr.Spec.ImagePullSecrets},
+	// }
+
+	// Define environment variables if needed
+	// envVars := r.getEnvVarsForPrometheusKafkaAdapter(cr)
+
+	// Define ports (use nil if not needed)
+	ports := []corev1.ContainerPort{
+		{ContainerPort: 3000, Protocol: corev1.ProtocolTCP},
+	}
+
+	// Liveness and Readiness Probes
+	livenessProbe := &corev1.Probe{
+		InitialDelaySeconds: 10,
+		PeriodSeconds:       30,
+		FailureThreshold:    3,
+		SuccessThreshold:    1,
+		TimeoutSeconds:      1,
+		ProbeHandler: corev1.ProbeHandler{
+			HTTPGet: &corev1.HTTPGetAction{
+				Path:   "/api/health",
+				Port:   intstr.FromInt(3000),
+				Scheme: corev1.URISchemeHTTPS,
+			},
+		},
+	}
+
+	readinessProbe := &corev1.Probe{
+		InitialDelaySeconds: 10,
+		PeriodSeconds:       30,
+		FailureThreshold:    3,
+		SuccessThreshold:    1,
+		TimeoutSeconds:      1,
+		ProbeHandler: corev1.ProbeHandler{
+			HTTPGet: &corev1.HTTPGetAction{
+				Path:   "/api/health",
+				Port:   intstr.FromInt(3000),
+				Scheme: corev1.URISchemeHTTPS,
+			},
+		},
+	}
+
+	strategy := &appsv1.DeploymentStrategy{
+		RollingUpdate: &appsv1.RollingUpdateDeployment{
+			MaxSurge:       &intstr.IntOrString{Type: intstr.String, StrVal: "25%"},
+			MaxUnavailable: &intstr.IntOrString{Type: intstr.String, StrVal: "25%"},
+		},
+	}
+
+	terminationGracePeriodSeconds := int64Ptr(30)
+
+	resources := corev1.ResourceRequirements{}
+
+	terminationMessagePath := "/dev/termination-log"
+
+	terminationMessagePolicy := corev1.TerminationMessagePolicy("File")
+
+	image := "docker.io/grafana/grafana:6.6.2"
+
+	// Define initContainers in a separate variable
+	initContainers := []corev1.Container{
+		{
+			Name:  "volume-mount",
+			Image: "busybox",
+			Command: []string{
+				"sh",
+				"-c",
+				"chmod -R 777 /grafanaData",
+			},
+			ImagePullPolicy: corev1.PullAlways,
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      "grafana-data",
+					MountPath: "/grafanaData",
+				},
+			},
+			TerminationMessagePath: terminationMessagePath,
+			TerminationMessagePolicy: terminationMessagePolicy,
+			Resources: resources,
+		},
+	}
+
+	return r.deployment(
+		strategy, // Deployment strategy
+		cr,
+		"orc8r-user-grafana",          // Name
+		labels,                        // Labels
+		nil,                           // Command
+		nil,                           // args (nil if not needed)
+		volumeMounts,                  // Volume mounts
+		volumes,                       // Volumes
+		ports,                         // Ports (empty if not needed)
+		initContainers,                // Init containers
+		nil,                           // DNS config
+		nil,                           // Annotations
+		nil,                           // Environment variables
+		livenessProbe,                 // Liveness probe
+		readinessProbe,                // Readiness probe
+		securityContext,               // Security context
+		corev1.DNSClusterFirst,        // DNS policy
+		corev1.RestartPolicyAlways,    // Restart policy
+		nil,                           // Image pull secrets
+		terminationGracePeriodSeconds, // terminationGracePeriodSeconds
+		corev1.PullIfNotPresent,       // Image pull policy
+		resources,                     // Resources
+		terminationMessagePath,        // Termination message path
+		terminationMessagePolicy,      // Termination message policy
+		image,                         // Image
+		nil,                           // Affinity
 	)
 }
